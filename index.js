dbdcbc03ca33feefddcedcb73c85ee14619094fa
Akil Tajwar <akiltajwar@gmail.com>
2024-04-06 20:24:09 +0600

    role create (not complete)

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorize access'});
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err) {
+      return res.status(401).send({error: true, message: 'Unauthorize access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdbahux.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -40,6 +56,10 @@ async function run() {
       if(req.query?.email) {
         query = {email: req.query.email}
       }
+      // const decodedEmail = req.decoded.email;
+      // if(req.query.email !== decodedEmail) {
+      //   return res.status(401).send({error: true, message: 'Unauthorize access'});
+      // }
       const result = await usersCollection.find(query).toArray();
       res.send(result);
     })
@@ -55,7 +75,18 @@ async function run() {
       res.send(insertedData);
     });
 
-    //maing admin role
+    app.get('/users/admin/:id', verifyJWT, async(req, res) => {
+      const email = req.params.email;
+      if(req.decoded.email !== email) {
+        res.send({admin: false})
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'};
+      res.send(result);
+    })
+
+    //making admin role
     app.patch('/users/admin/:id', async(req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
