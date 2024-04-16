6b9bab2260eb2e4dda65cf1ac45f6ecd8cf469fb
Akil Tajwar <akiltajwar@gmail.com>
2024-04-17 03:33:28 +0600

    delete method on my jobs (not complete yet)

--- index.js
+++ index.js
@@ -127,6 +127,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/myJobPosts/:id', async (req, res) => {
+      const id = req.params.id;
+      const querry = { _id: new ObjectId(id) };
+      const result = await jobPostCollection.deleteOne(querry);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
